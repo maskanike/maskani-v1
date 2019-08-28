240e37bba3ea0bf3d0258778ae633494ab3a98fb
Samuel Magondu <magondunjenga@gmail.com>
2019-08-28 20:25:10 +0300

    Add Login

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,11 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Maskani' });
 });
 
+/* GET Login */
+router.get('/login', function(req, res, next) {
+  res.render('login', { user: 'Eugene' });
+});
+
 router.get('/health', function(req, res, next) {
   res.send('I am healthy');
 });
