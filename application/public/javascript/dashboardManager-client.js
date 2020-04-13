217f72b33841f84dd8a9b221cf69b27ffeda92ef
Samuel Magondu <magondunjenga@gmail.com>
2020-04-13 12:02:32 +0300

    Change dashboard to line graph

--- application/public/javascript/dashboardManager-client.js
+++ application/public/javascript/dashboardManager-client.js
@@ -21,7 +21,6 @@ function getDashboardData() {
   });
 
   request.done(function (data) {
-    console.log(data);
     const { invoicedAmounts, monthStats } = data
     drawChart(invoicedAmounts);
     populateDashboardStats(monthStats);
@@ -40,39 +39,34 @@ function populateDashboardStats(data) {
 function drawChart(data) {
   var ctx = $('#dashboardChart');
   var myChart = new Chart(ctx, {
-    type: 'bar',
+    type: 'line',
     data: {
       labels: data.labels,
       datasets: [{
         label: 'Amount Invoiced',
         data: data.data,
+        fill: false,
         backgroundColor: [
-          'rgba(255, 99, 132, 0.2)',
-          'rgba(54, 162, 235, 0.2)',
-          'rgba(255, 206, 86, 0.2)',
+          // 'rgba(255, 99, 132, 0.2)',
+          // 'rgba(54, 162, 235, 0.2)',
+          // 'rgba(255, 206, 86, 0.2)',
           'rgba(75, 192, 192, 0.2)',
-          'rgba(153, 102, 255, 0.2)',
-          'rgba(255, 159, 64, 0.2)'
+          // 'rgba(153, 102, 255, 0.2)',
+          // 'rgba(255, 159, 64, 0.2)'
         ],
         borderColor: [
-          'rgba(255, 99, 132, 1)',
-          'rgba(54, 162, 235, 1)',
-          'rgba(255, 206, 86, 1)',
+          // 'rgba(255, 99, 132, 1)',
+          // 'rgba(54, 162, 235, 1)',
+          // 'rgba(255, 206, 86, 1)',
           'rgba(75, 192, 192, 1)',
-          'rgba(153, 102, 255, 1)',
-          'rgba(255, 159, 64, 1)'
+          // 'rgba(153, 102, 255, 1)',
+          // 'rgba(255, 159, 64, 1)'
         ],
-        borderWidth: 1
+        borderWidth: 2
       }]
     },
     options: {
-      scales: {
-        yAxes: [{
-          ticks: {
-            beginAtZero: true
-          }
-        }]
-      }
+      responsive: true,
     }
   });
 }
